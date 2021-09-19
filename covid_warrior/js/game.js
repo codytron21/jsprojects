7ade3e16fa79d47f69bf53754ad1a76e1e85b700
rohan <rohannak2057@gmail.com>
2021-09-19 14:52:03 +0530

    added enemy image

--- covid_warrior/js/game.js
+++ covid_warrior/js/game.js
@@ -1,6 +1,7 @@
 function load_image() {
     // player,virus,gem
-
+    enemy_image = new Image;
+    enemy_image.src = "../assets/v1.png"
 }
 function init() {
     //define the objects that we will have in the game.
@@ -11,13 +12,28 @@ function init() {
     canvas.height = H;
     pen = canvas.getContext('2d');
 
-    box = {
+    e1 = {
         x: 150,
         y: 50,
         w: 60,
         h: 60,
         speed: 20
     };
+    e2 = {
+        x: 300,
+        y: 150,
+        w: 60,
+        h: 60,
+        speed: 30
+    };
+    e3 = {
+        x: 450,
+        y: 20,
+        w: 60,
+        h: 60,
+        speed: 40
+    };
+    enemy = [e1, e2, e3];
 }
 //to prints the objects on the screen
 function draw() {
@@ -25,14 +41,24 @@ function draw() {
     pen.clearRect(0, 0, W, H);
 
     pen.fillStyle = "red";
-    pen.fillRect(box.x, box.y, box.w, box.h);
+    // pen.fillRect(box.x, box.y, box.w, box.h);
+    // pen.drawImage(enemy_image, box.x, box.y, box.w, box.h);
+    for (let i = 0; i < enemy.length; i++) {
+        pen.drawImage(enemy_image, enemy[i].x, enemy[i].y, enemy[i].w, enemy[i].h);
+    }
 }
 //to add movement to the object
 function update() {
 
-    box.y += box.speed;
-    if (box.y >= H - box.h || box.y < 0) {
-        box.speed *= -1;
+    // box.y += box.speed;
+    // if (box.y >= H - box.h || box.y < 0) {
+    //     box.speed *= -1;
+    // }
+    for (let i = 0; i < enemy.length; i++) {
+        enemy[i].y += enemy[i].speed;
+        if (enemy[i].y >= H - enemy[i].h || enemy[i].y < 0) {
+            enemy[i].speed *= -1;
+        }
     }
 }
 function gameloop() {
