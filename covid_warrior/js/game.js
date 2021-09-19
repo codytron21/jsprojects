584c847fb4f1418a658240449bc45e35434c3a11
rohan <rohannak2057@gmail.com>
2021-09-19 15:56:34 +0530

    added collosion detection

--- covid_warrior/js/game.js
+++ covid_warrior/js/game.js
@@ -13,6 +13,7 @@ function init() {
     canvas = document.getElementById("mycanvas");
     H = 400;
     W = 700;
+    gameOver = false;
     canvas.width = W;
     canvas.height = H;
     pen = canvas.getContext('2d');
@@ -45,7 +46,8 @@ function init() {
         w: 60,
         h: 60,
         speed: 20,
-        moving: false
+        moving: false,
+        health: 100
     }
     gem = {
         x: W - 100,
@@ -63,12 +65,23 @@ function init() {
         player.moving = false;
     });
 }
+function isCollision(rect1, rect2) {
+    if (rect1.x < rect2.x + rect2.w &&
+        rect1.x + rect1.w > rect2.x &&
+        rect1.y < rect2.y + rect2.h &&
+        rect1.y + rect1.h > rect2.y) {
+        return true;
+    }
+    return false;
+}
 //to prints the objects on the screen
 function draw() {
     //clear the canvas area of the old frame
     pen.clearRect(0, 0, W, H);
 
-    pen.fillStyle = "red";
+    pen.fillStyle = "white";
+    pen.font = '20px Arial';
+    pen.fillText("Score " + player.health, 10, 20);
     // pen.fillRect(box.x, box.y, box.w, box.h);
     // pen.drawImage(enemy_image, box.x, box.y, box.w, box.h);
     pen.drawImage(player_image, player.x, player.y, player.w, player.h);
@@ -83,6 +96,19 @@ function update() {
     if (player.moving == true) {
         player.x += player.speed;
     }
+    if (isCollision(player, gem)) {
+        player.health += 20;
+        alert("YOU WON" + player.health);
+        return;
+    }
+    for (let i = 0; i < enemy.length; i++) {
+        if (isCollision(player, enemy[i])) {
+            player.health -= 101;
+        }
+        if (player.health < 0) {
+            gameOver = true;
+        }
+    }
     // box.y += box.speed;
     // if (box.y >= H - box.h || box.y < 0) {
     //     box.speed *= -1;
@@ -95,6 +121,9 @@ function update() {
     }
 }
 function gameloop() {
+    if (gameOver) {
+        clearInterval(f);
+    }
     draw();
     update();
     console.log("In gameloop");
