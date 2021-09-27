b0fded04b1a47f8a65ad682370647f4bb74b9dfc
rohan <rohannak2057@gmail.com>
2021-09-27 22:10:08 +0530

    added components

--- spin&win/js/game.js
+++ spin&win/js/game.js
@@ -1,7 +1,7 @@
 let config = {
     type: Phaser.CANVAS,
     width: 800,
-    height: 600,
+    height: 400,
 
     scene: {
         preload: preload,
@@ -14,11 +14,13 @@ let game = new Phaser.Game(config);
 function preload() {
     console.log("preload");
     // we have lots of objects  and methods provided by the phaser frame work.
-    // "this" keyword is used as preload is part of scene object.
+    // "this" keyword is used, as preload is part of scene object.
     //load is a phaser object in the scene object.
     //we can give the name of the image here we have given it name 'background'
     this.load.image('background', '../assets/back.jpg');
-
+    this.load.image('wheel', '../assets/wheel.png');
+    this.load.image('pin', '../assets/pin.png');
+    this.load.image('stand', '../assets/stand.png');
 }
 function create() {
     console.log("create");
@@ -27,10 +29,22 @@ function create() {
     let H = game.config.height;
     // sprite a object in scene object from the phaser.
     //sprite is a way to create images.
+    /*sprite(0,0,'name') 0,0 in sprite is the position where to load the image*/
+
     let background = this.add.sprite(0, 0, 'background');
     // setPostion,setScales are method in spirte.
     background.setPosition(W / 2, H / 2);
     background.setScale(0.20);
+    let stand = this.add.sprite(W / 2, H / 2 + 120, 'stand');
+    stand.setScale(0.30);
+    let wheel = this.add.sprite(W / 2, H / 2, 'wheel');
+    wheel.setScale(0.12);
+
+    let pin = this.add.sprite(W / 2, H / 2 - 130, 'pin');
+    pin.setScale(0.20);
+
+
+
 }
 function update() {
     console.log("update");
