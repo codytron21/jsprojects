40da4855eafcf920f1fe710f517913aa1146a798
rohan <rohannak2057@gmail.com>
2021-10-03 22:16:26 +0530

    added player and gravity

--- mario/js/game.js
+++ mario/js/game.js
@@ -2,10 +2,21 @@ let config = {
     type: Phaser.AUTO,
     scale: {
         mode: Phaser.Scale.FIT,
-        width: 800,
-        height: 600,
+        width: 1300,
+        height: 610,
     },
+
     backgroundColor: 0xffffcc,
+    // physics engines by phaser
+    //one of basic physics engine is arcade-- it can be used to give speed ,gravity etc.
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 1000,
+            }
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -15,6 +26,9 @@ let config = {
 let game = new Phaser.Game(config);
 function preload() {
     this.load.image("ground", "assets/topground.png");
+    this.load.image("sky", "assets/background.png");
+    //to a sprite map type of image we use spritesheet func.
+    this.load.spritesheet("dude", "assets/dude.png", { frameWidth: 32, frameHeight: 48 });
 }
 function create() {
     W = game.config.width;
@@ -26,6 +40,23 @@ function create() {
     //every image have center as there origin,accordingly it is drawn on the screen.
     //setOrigin gives us capibility to change the position from center to anywhere.
     ground.setOrigin(0, 0);
+    // adding background
+    let background = this.add.sprite(0, 0, 'sky');
+    background.setOrigin(0, 0);
+    background.displayWidth = W;
+    background.depth = -1;
+    //to make the player an object on which we can apply physics we use .physics
+    let player = this.physics.add.sprite(100, 100, 'dude', 4);
+    //to make ground sprite object experience physics we add .pyhicsand since we have ground sprite already we use "existing() "
+    this.physics.add.existing(ground);
+    //exempting ground ground from gravity
+    ground.body.allowGravity = false;
+    ground.body.immovable = true;
+    //add a collision detection btw player and ground.
+    this.physics.add.collider(ground, player);
+
+
+
 }
 function update() {
 
